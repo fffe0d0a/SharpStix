46b6bc8be13e226c9a5aea559063e813db90ab2a
FFFE0D0A <fffe0d0a@users.noreply.github.com>
2023-05-15 13:30:33 +0100

    Partially improve GranularMarking

---
+++ SharpStix/Serialisation/Json/Converters/Objects/SelectorConverter.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using SharpStix.StixObjects;
+
+namespace SharpStix.Serialisation.Json.Converters;
+
+public class SelectorConverter : JsonConverter<Selector>
+{
+    public override Selector? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        string? value = reader.GetString();
+        return value == null
+            ? null :
+            new Selector(value);
+    }
+
+    public override void Write(Utf8JsonWriter writer, Selector value, JsonSerializerOptions options) => writer.WriteStringValue(value.ToString());
+}
--- SharpStix/StixObjects/LocalTypes/GranularMarking.cs
+++ SharpStix/StixObjects/LocalTypes/GranularMarking.cs
@@ -1,14 +1,9 @@
 ﻿using SharpStix.StixTypes;
 using System.Globalization;
+using System.Text.Json.Serialization;
+using SharpStix.Serialisation.Json.Converters;
 
 namespace SharpStix.StixObjects;
-
-//todo
-
-//Selectors contained in the selectors list are strings that consist of multiple components that MUST be separated by the.character.Each component MUST be one of:
-
-//●	A property name or dictionary key, e.g., description, or;
-//●	A zero-based list index, specified as a non-negative integer in square brackets, e.g., [4]
 
 public record GranularMarking
 {
@@ -16,5 +11,22 @@ public record GranularMarking
 
     public CultureInfo? Lang { get; set; }
     public StixIdentifier? MarkingRef { get; set; }
-    public required StixList<string> Selectors { get; set; } //Selectors contained in the selectors list are strings that consist of multiple components that MUST be separated by the . character.
+    public required StixList<Selector> Selectors { get; set; }
+}
+
+//todo improve abstraction
+//Selectors contained in the selectors list are strings that consist of multiple components that MUST be separated by the.character.Each component MUST be one of:
+//●	A property name or dictionary key, e.g., description, or;
+//●	A zero-based list index, specified as a non-negative integer in square brackets, e.g., [4]
+[JsonConverter(typeof(SelectorConverter))]
+public record Selector
+{
+    public Selector(string value)
+    {
+        Values = value.Split('.', StringSplitOptions.RemoveEmptyEntries);
+    }
+
+    public string[] Values { get; }
+
+    public override string ToString() => string.Join('.', Values);
 }
